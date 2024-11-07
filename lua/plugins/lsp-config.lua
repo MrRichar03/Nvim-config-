873db2781ff6156d7577cef9c79dd3a005228ab4
MrRichar03 <richarmadina03@gmail.com>
2024-11-07 06:34:19 -0500

    feat(plugins): auto install lsp
    i added into the config something that makes sure to install the lsp
    servers that are need

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,22 +1,24 @@
 return {
   {
     "williamboman/mason.nvim",
+		lazy = false,
     config = function()
       require("mason").setup()
     end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "pyright", "tsserver" }
-      })
-    end
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
   },
-  {  
+  {
     "neovim/nvim-lspconfig",
+		lazy = false,
     config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
 				capabilities = capabilities
@@ -27,10 +29,16 @@ return {
       lspconfig.tsserver.setup({
 				capabilities = capabilities
 			})
+      lspconfig.jdtls.setup({
+				capabilities = capabilities
+			})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>gD', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+			vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     end
   }
 }
