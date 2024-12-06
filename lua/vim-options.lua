0c24c3c22aa6e8d5e5e6888dabb6ffd1912aae7f
MrRichar03 <richarmadina03@gmail.com>
2024-12-06 12:36:09 -0500

    feat(root): relative number
    i added relative numbers to navigate faster

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set mouse=nv")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
